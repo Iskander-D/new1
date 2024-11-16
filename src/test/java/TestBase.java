c5e1fe154f084a94985d38c5d23386f84be43578
iskander-d <drozenko21@gmail.com>
2024-11-16 15:52:16 +0300

    new

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,54 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = System.getProperty("browserSize", "1330x890");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "129");
+        Configuration.remote = System.getProperty("urlHost");
+        Configuration.baseUrl = "https://www.gazprombank.ru/";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 10000;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @BeforeEach
+    void allureSelenide() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.pageSource();
+        Attach.screenshotAs("Last screenshot");
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
+
+//    @AfterEach
+//    void closeWebDriver() {
+//        WebDriverRunner.closeWebDriver();
+//    }
+//}
