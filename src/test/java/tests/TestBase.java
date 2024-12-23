a92eccbaf2233a76f12df7cad90cf1da36f69819
iskander-d <drozenko21@gmail.com>
2024-12-23 17:55:21 +0500

    1 еуtest-2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,14 +22,14 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.browserVersion = "130";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
